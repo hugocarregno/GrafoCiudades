2ca93c8bdd0478228c848ebc50add61499eaeb0c
Hugo <hugo.carregno@gmail.com>
2020-08-24 22:41:19 -0300

    se agregaron ciudades de forma inversa

--- Main.java
+++ Main.java
@@ -15,13 +15,21 @@ class Main {
     grafo.nuevoVertice("Las Isletas");
 
     grafo.nuevoArco("La Toma","San Luis",80);
+    grafo.nuevoArco("San Luis","La Toma",80);
     grafo.nuevoArco("La Toma","El Morro",29);
+    grafo.nuevoArco("El Morro","La Toma",29);
     grafo.nuevoArco("San Luis","Fraga",56);
+    grafo.nuevoArco("Fraga","San Luis",56);
     grafo.nuevoArco("El Morro","Villa Mercedes",54);
+    grafo.nuevoArco("Villa Mercedes","El Morro",54);
     grafo.nuevoArco("Fraga","Villa Mercedes",36);
+    grafo.nuevoArco("Villa Mercedes","Fraga",36);
     grafo.nuevoArco("Villa Mercedes","Las Isletas",21);
+    grafo.nuevoArco("Las Isletas","Villa Mercedes",21);
     grafo.nuevoArco("Villa Mercedes","Justo Daract",40);
+    grafo.nuevoArco("Justo Daract","Villa Mercedes",40);
     grafo.nuevoArco("Las Isletas","Justo Daract",37);
+    grafo.nuevoArco("Justo Daract","Las Isletas",37);
 
     
     grafo.imprimir(); 
